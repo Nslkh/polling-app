800509d62c6baab4c355dea6474a9b72fb34a868
Narzullo <olluzran@gmail.com>
2021-02-13 12:49:45 +0300

    template header & footer added

--- functions.php
+++ functions.php
@@ -12,3 +12,33 @@ function pdo_connect_mysql() {
         die ('Failed to connect to database!');
     }
 }
+
+// Template header, feel free to customize this
+function template_header($title) {
+    return <<<EOT
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>$title</title>
+        <link href="style.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+    </head>
+    <body>
+    <nav class="navtop">
+        <div>
+            <h1>Voting & Poll System</h1>
+            <a href="index.php"><i class="fas fa-poll-h"></i>Polls</a>
+        </div>
+    </nav>
+EOT;
+}
+
+
+// Template footer
+function template_footer() {
+    return <<<EOT
+    </body>
+</html>
+EOT;
+}
